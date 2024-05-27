e86468f978f0010481b11e2f2ed5c9a8c3e9a9c1
Parmod Kumar <yparmodyadav@gmail.com>
2024-05-27 22:01:30 +0530

    Fixes

--- admin panel/src/components/Auth/Login.jsx
+++ admin panel/src/components/Auth/Login.jsx
@@ -104,7 +104,7 @@ const Login = () => {
           </button>
         </form>
         <p className="mt-4 text-center select-none">
-          Don't have an account?{' '}
+          Don&apos;t have an account?{' '}
           <Link to="/register" className="text-blue-500">
             Register
           </Link>
